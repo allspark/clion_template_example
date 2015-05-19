83dfb83ec5212e2193030adacda168549b12dbe1
Dennis Boerm <allspark@wormhole.eu>
2015-05-19 17:33:48 +0200

    template example

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <utility>
+#include <tuple>
+
+using namespace std;
+
+class Base {
+  public:
+    virtual ~Base() = default;
+};
+
+class Derived : public Base {
+
+};
+
+std::tuple<int, int, float> getValues() {
+    return std::make_tuple(5,23,42.0);
+}
+
+int main() {
+    {
+        int a, b;
+        float c;
+
+        std::tie(a, b, c) = getValues();
+    }
+
+    {
+        std::shared_ptr<Base> b = std::make_shared<Derived>();
+
+        std::shared_ptr<Derived> d = std::dynamic_pointer_cast<Derived>(b);
+    }
+
+
+    return 0;
+}
